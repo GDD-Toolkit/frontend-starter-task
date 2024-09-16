f79e8e262e2d71f18d8e6cf739ed4d52c41842db
Sai Seyyadri <sai.seyyadri@gmail.com>
2024-09-16 14:09:24 -0400

    Fixing colors and corresponding incrementations

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ let blueCount = 0;
 function changeBackgroundColor(color) {
     document.body.style.backgroundColor = color;
     if (color === '#f55345') {
-        redCount++;
+        redCount = redCount + 1;
         document.getElementById('red-count').innerText = redCount;
     } else if (color === '#f6cf59') { 
-        yellowCount++;
+        yellowCount = yellowCount + 1;
         document.getElementById('yellow-count').innerText = yellowCount;
     } else if (color === '#7cea9b') { 
-        greenCount++;
+        greenCount = greenCount + 1;
         document.getElementById('green-count').innerText = greenCount;
     } else if (color === '#3274de') { 
-        blueCount++;
+        blueCount = blueCount + 1;
         document.getElementById('blue-count').innerText = blueCount;
     }
 }
@@ -35,19 +35,19 @@ function clearBoard() {
 }
 
 document.getElementById('red-btn').addEventListener('click', function() {
-    changeBackgroundColor('#f28b82');
+    changeBackgroundColor('#f55345');
 });
 
 document.getElementById('yellow-btn').addEventListener('click', function() {
-    changeBackgroundColor('#fdd663');
+    changeBackgroundColor('#f6cf59');
 });
 
 document.getElementById('green-btn').addEventListener('click', function() {
-    changeBackgroundColor('#81c995');
+    changeBackgroundColor('#7cea9b');
 });
 
 document.getElementById('blue-btn').addEventListener('click', function() {
-    changeBackgroundColor('#82b1ff');
+    changeBackgroundColor('#3274de');
 });
 
 document.getElementById('clear-btn').addEventListener('click', clearBoard);
